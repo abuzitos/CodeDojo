ea422d6ca2c8146998912c7866b81020c38bb889
Nelson Abu <abuzitos@gmail.com>
2025-01-04 12:43:14 +0100

    feat: add commit message template and update Java version to 17

---
+++ src/main/java/org/codedojo/projetoIA/bank/comandos.sql
@@ -0,0 +1,15 @@
+// inserir produtos na tabela de banco de dados chamada produtos
+
+
+
+// gerar dados de 50 estudantes com id, nome, aprovado em formato .csv
+// id,nome,aprovado
+// 1,João,Sim
+// 2,Maria,Não
+// 3,Carlos,Sim
+// 4,Ana,Não
+// 5,Paulo,Sim
+// 6,Julia,Não
+// 7,Roberto,Sim
+// 8,Carla,Não
+// 9,Joana,Sim
